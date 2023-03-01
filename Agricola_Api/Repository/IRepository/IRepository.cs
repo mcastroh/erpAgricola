adccb3c94d6d44384fcbc290a89e5dd1fd618250
Marco Castro <m_castrohinostroza@hotmail.com>
2023-03-01 13:58:13 -0500

    Api Empresas y Locales

--- Agricola_Api/Repository/IRepository/IRepository.cs
+++ Agricola_Api/Repository/IRepository/IRepository.cs
@@ -4,15 +4,14 @@ namespace Agricola_Api.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        Task Crear(T entidad);
-
         Task<List<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null);
 
         Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true);
 
-        Task Remover(T entidad);
+        Task Crear(T entidad);
 
         Task Grabar();
 
+        Task Remover(T entidad);
     }
 }
--- Agricola_Api/Repository/Repository.cs
+++ Agricola_Api/Repository/Repository.cs
@@ -10,30 +10,17 @@ namespace Agricola_Api.Repository
         private readonly ApplicationDbContext _context;
         internal DbSet<T> _dbSet;
 
+        #region Constructor
+
         public Repository(ApplicationDbContext context) 
         {
             _context = context;
             _dbSet = _context.Set<T>();
         }
 
-        public async Task Crear(T entidad)
-        {
-            await _dbSet.AddAsync(entidad);
-            await Grabar();
-        }
-
-        public async Task Grabar()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true)
-        {
-            IQueryable<T> query = _dbSet;
-            if(!tracked) { query = query.AsNoTracking(); }
-            if(filtro != null) { query = query.Where(filtro); }
-            return await query.FirstOrDefaultAsync();
-        }
+        #endregion
+        
+        #region ObtenerTodos
 
         public async Task<List<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null)
         {
@@ -42,10 +29,48 @@ namespace Agricola_Api.Repository
             return await query.ToListAsync();
         }
 
+        #endregion
+
+        #region Obtener
+
+        public async Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (!tracked) { query = query.AsNoTracking(); }
+            if (filtro != null) { query = query.Where(filtro); }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        #endregion
+
+        #region Crear
+
+        public async Task Crear(T entidad)
+        {
+            await _dbSet.AddAsync(entidad);
+            await Grabar();
+        }
+
+        #endregion
+
+        #region Grabar
+
+        public async Task Grabar()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion 
+     
+        #region Remover
+
         public async Task Remover(T entidad)
         {
             _dbSet.Remove(entidad);
             await Grabar();
         }
+
+        #endregion 
+
     }
 }
